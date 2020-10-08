19b8ab96470df5d46fadc075abe791e41f9e53c8
김홍규 <Hongkyu@dhcp-wlan-eduroam-10-16-53-170.uzh.ch>
2020-10-08 16:10:10 +0200

    Queue, lowest value gets removed first

--- Queue.py
+++ Queue.py
@@ -1,7 +1,7 @@
 class Queue:
 	'''
 	Implementation of Queue using python dictionary.
-	Initially key is given as key = 0, -1, -2, -3, -4, ... with highest key is removed first.
+	Initially key is given as key = 0, 1, 2, 3, 4, ... with lowest key is removed first.
 	As dequeue() is applied, we decrease the initial index (init_idx) one by one.
 	'''
 
@@ -16,14 +16,14 @@ class Queue:
 	def __repr__(self):
 		repr_str = "<- Front "
 		for idx in range(self.size):
-			val = self.queue[self.init_idx - idx]
+			val = self.queue[self.init_idx + idx]
 			repr_str += str(val) + " "
 		repr_str += "End"
 		return repr_str
 	
 
 	def enqueue(self, val):
-		self.queue[-self.size] = val
+		self.queue[self.init_idx + self.size] = val
 		self.size += 1
 
 	def dequeue(self):
@@ -32,7 +32,7 @@ class Queue:
 		
 		# Retrieve the first value using init_idx and update it
 		first_val = self.queue.pop(self.init_idx)
-		self.init_idx -= 1
+		self.init_idx += 1
 		self.size -= 1
 		return first_val
 		
@@ -49,13 +49,17 @@ if __name__ == '__main__':
 	myQueue.enqueue(2)
 	myQueue.enqueue(3)
 	print(myQueue)	# <- Front 1 2 3 End
+	print(myQueue.init_idx)		# 0
 
 
 	print(myQueue.peek())		# 1
 	print(myQueue.dequeue())	# 1
+	print(myQueue.init_idx)		# 1
+	myQueue.enqueue(5)
+	print(myQueue)			# <- Front 2 3 5 End	
 
 	while myQueue:
-		print(myQueue.dequeue())	# 2 3
+		print(myQueue.dequeue())	# 2 3 5
 
 	print("Below shows the result of dequeue on empty Queue")
 	myQueue.dequeue()	# Error "Can't dequeue from an empty Queue"
